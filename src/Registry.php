e3fbabb2c827d52019da83943e1d9c1fa937fa90
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2023-09-25 20:40:01 +0300

    Initial commit

---
+++ src/Registry.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace Registry;
+
+class Registry
+{
+    private static $_instances = array();
+
+    public function __construct()
+    {        
+    }
+
+    public function __clone()
+    {        
+    }
+
+    public static function get($key, $default = null)
+    {
+        if (isset(self::$_instances[$key])) {
+            return self::$_instances[$key];
+        }
+        return $default;
+    }
+
+    public static function set($key, $instance = null)
+    {
+        self::$_instances[$key] = $instance;
+    }
+
+    public static function erase($key)
+    {
+        unset(self::$_instances[$key]);
+    }
+}
